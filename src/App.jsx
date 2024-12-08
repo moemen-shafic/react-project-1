ce0506907f77a1779c53618ee28ca9ee1c572c18
Abdelrahman207 <abdelrahmanshafiq207@gmail.com>
2024-12-08 15:23:42 +0200

    second upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import "./Theme.css";
 
 function App() {
 
@@ -11,33 +12,59 @@ function App() {
   const chageAge = () => {
     setAge(37)
   }
-  
-const changeCount = () => {
-  setCount(count + 1)
-}
 
+  const changeCount = () => {
+    setCount(count + 1)
+  }
+
+
+  //==================== change mode ===///
+
+  const [theme, settheme] = useState("dark");
   return (
-    <div className="App">
+    <div className={`App ${theme}`}>
+
+
+      <div>
+        <button className="light" type="button" onClick={() => {
+          settheme("light")
+        }
+        }>light</button>
+        <button style={{ marginLeft: "20px" }} type="button" onClick={() => {
+          settheme("dark")
+        }
+        }>dark</button>
+        <button style={{ marginLeft: "20px" }} type="button" onClick={() => {
+          settheme("yellow")
+        }
+        }>yellow</button>
+        <button style={{ marginLeft: "20px" }} type="button" onClick={() => {
+          settheme("blue")
+        }
+        }>blue</button>
+
+      </div>
+
       <h1>My name is {person}</h1>
       <button type="button" onClick={() => {
         setPerson("moemen Ahmed Shafic")
       }
       }>change name</button>
-<br/>
-<br/>
+      <br />
+      <br />
 
-<h1>my age is {age}</h1>
-<button onClick={chageAge}>change Age</button>
+      <h1>my age is {age}</h1>
+      <button onClick={chageAge}>change Age</button>
 
-<br/>
-<br/>
-<br/>
+      <br />
+      <br />
+      <br />
 
-<button onClick={ changeCount }>count is {count}</button>
+      <button onClick={changeCount}>count is {count}</button>
 
     </div>
 
-    
+
   );
 }
 
